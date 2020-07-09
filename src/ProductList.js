c6ca3b7215822a20db1c794921f1e89c89ac2591
fsuffieldcode <fsuffieldcode@gmail.com>
2020-07-09 13:54:00 +0100

    create ProductList component

---
+++ src/ProductList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Inventory from './Inventory';
+
+export default class ProductList extends Component {
+	render() {
+		return (
+			<section>
+				<div></div>
+			</section>
+		);
+	}
+}
